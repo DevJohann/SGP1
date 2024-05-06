4a34ef2c133be266c93c793185cf0e5e0274744f
DataLab Biblioteca <datalabueb@unbosque.edu.co>
2024-05-06 17:27:04 -0500

    initial commit

---
+++ src/main/java/co/rootsoftworks/app/sgp1/sgp1/Sgp1Application.java
@@ -0,0 +1,13 @@
+package co.rootsoftworks.app.sgp1.sgp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sgp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sgp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/co/rootsoftworks/app/sgp1/sgp1/Sgp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.rootsoftworks.app.sgp1.sgp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sgp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
